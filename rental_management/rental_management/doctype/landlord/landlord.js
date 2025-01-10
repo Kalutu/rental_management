b5f4b4bad8a0af2738506d5d526f9b1faffbe998
Kalutu <kalutudaniel@gmail.com>
2025-01-10 14:25:39 +0300

    feat: landlord is a company

--- rental_management/rental_management/doctype/landlord/landlord.js
+++ rental_management/rental_management/doctype/landlord/landlord.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2025, Kalutu and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Landlord", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Landlord", {
+	after_save(frm) {
+		frappe.call({
+			method: "rental_management.rental_management.doctype.landlord.landlord.create_company",
+			args: {
+				name: frm.doc.name,
+				landlord_name: frm.doc.landlord_name,
+			},
+			callback: (r) => {
+				console.log("Landlord details fetched");
+			},
+		});
+	},
+});
--- rental_management/rental_management/doctype/landlord/landlord.py
+++ rental_management/rental_management/doctype/landlord/landlord.py
@@ -1,9 +1,28 @@
 # Copyright (c) 2025, Kalutu and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Landlord(Document):
 	pass
+
+@frappe.whitelist()
+def create_company(**args):
+	name = args.get('name')
+	landlord_name = args.get('landlord_name')
+
+	# create a new document
+	company = frappe.get_doc({
+		'doctype': 'Customer',
+		'customer_name': landlord_name,
+		'customer_type': "Company",
+	})
+	company.insert()
+
+	company_doc = frappe.get_last_doc("Customer")
+
+	landlord_doc =frappe.get_doc("Landlord",name)
+	landlord_doc.company = company_doc.customer_name
+	landlord_doc.save(ignore_permissions = True)
