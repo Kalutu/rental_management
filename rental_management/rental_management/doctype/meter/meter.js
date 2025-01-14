79b8c57929df3e4e0a7d7ecf9fc775166fb5f028
Kalutu <kalutudaniel@gmail.com>
2025-01-14 10:40:38 +0300

    feat: metring

--- rental_management/rental_management/doctype/meter/meter.js
+++ rental_management/rental_management/doctype/meter/meter.js
@@ -1,8 +1,89 @@
 // Copyright (c) 2025, Kalutu and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Meter", {
-// 	refresh(frm) {
+frappe.ui.form.on("Meter", {
+	refresh(frm) {
+		frm.set_query("unit", () => {
+			return {
+				filters: {
+					property: frm.doc.property,
+				},
+			};
+		});
+	},
 
-// 	},
-// });
+	property(frm) {
+		frappe.call({
+			method: "rental_management.rental_management.doctype.meter.meter.get_initial_reading",
+			args: {
+				meter_method: frm.doc.meter_method,
+				property: frm.doc.property,
+				unit: frm.doc.unit,
+				meter_type: frm.doc.meter_type,
+			},
+			callback: (r) => {
+				console.log(r.message);
+				frm.set_value("initial_reading", r.message);
+			},
+		});
+	},
+	unit(frm) {
+		frappe.call({
+			method: "rental_management.rental_management.doctype.meter.meter.get_initial_reading",
+			args: {
+				meter_method: frm.doc.meter_method,
+				property: frm.doc.property,
+				unit: frm.doc.unit,
+				meter_type: frm.doc.meter_type,
+			},
+			callback: (r) => {
+				console.log(r.message);
+				frm.set_value("initial_reading", r.message);
+			},
+		});
+	},
+
+	meter_type(frm) {
+		frappe.call({
+			method: "rental_management.rental_management.doctype.meter.meter.get_initial_reading",
+			args: {
+				unit: frm.doc.unit,
+				meter_type: frm.doc.meter_type,
+			},
+			callback: (r) => {
+				console.log(r.message);
+				frm.set_value("initial_reading", r.message);
+			},
+		});
+	},
+
+	// initial_reading(frm) {
+	// 	frm.set_value("token_used", frm.doc.current_reading - frm.doc.initial_reading);
+	// },
+
+	current_reading(frm) {
+		frm.set_value("token_used", frm.doc.current_reading - frm.doc.initial_reading);
+		if (frm.doc.meter_method === "Full Property") {
+			frappe.call({
+				method: "rental_management.rental_management.doctype.meter.meter.get_unit_count",
+				args: {
+					property: frm.doc.property,
+				},
+				callback: (r) => {
+					if (r.message) {
+						const unit_count = r.message;
+						console.log(`Unit count: ${unit_count}`);
+
+						// Calculate token_per_unit
+						if (unit_count > 0) {
+							frm.set_value("token_per_unit", frm.doc.token_used / unit_count);
+						} else {
+							frappe.msgprint(__("No units found for the selected property."));
+							frm.set_value("token_per_unit", 0);
+						}
+					}
+				},
+			});
+		}
+	},
+});
--- rental_management/rental_management/doctype/meter/meter.py
+++ rental_management/rental_management/doctype/meter/meter.py
@@ -1,9 +1,59 @@
 # Copyright (c) 2025, Kalutu and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Meter(Document):
 	pass
+
+@frappe.whitelist()
+def get_initial_reading(**args):
+	meter_method = args.get('meter_method')
+	asset = args.get('property')
+	unit = args.get('unit')
+	meter_type = args.get('meter_type')
+
+	if meter_method == "Per Unit":
+		# Check for an existing meter record with the same property, unit, and meter type
+		previous_meter = frappe.get_all(
+			'Meter',
+			filters={
+				'unit': unit,
+				'meter_type': meter_type
+			},
+			fields=['current_reading'],
+			order_by='creation desc',
+			limit_page_length=1
+		)
+		
+		# If a previous meter exists, set the initial reading from the current reading
+		if previous_meter:
+			return previous_meter[0].current_reading or 0
+		return 0
+
+	elif meter_method == "Full Property":
+		# Check for an existing meter record with the same property and meter type
+		previous_meter = frappe.get_all(
+			'Meter',
+			filters={
+				'property': asset,
+				'meter_type': meter_type
+			},
+			fields=['current_reading'],
+			order_by='creation desc',
+			limit_page_length=1
+		)
+		
+		# If a previous meter exists, set the initial reading from the current reading
+		if previous_meter:
+			return previous_meter[0].current_reading or 0
+		return 0
+		
+@frappe.whitelist()
+def get_unit_count(property):
+    if not property:
+        frappe.throw(_("Property is required to fetch the number of units."))
+    # Count the number of units linked to the property
+    return frappe.db.count("Unit", {"property": property})
