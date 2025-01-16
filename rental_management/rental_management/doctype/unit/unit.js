96307547a82f04ce09255a41d5d132afc165ecc6
Kalutu <kalutudaniel@gmail.com>
2025-01-16 11:00:57 +0300

    refactor: unit doctype

--- rental_management/rental_management/doctype/unit/unit.js
+++ rental_management/rental_management/doctype/unit/unit.js
@@ -2,44 +2,28 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Unit", {
-	property: function (frm) {
-		// Triggered when `property` is changed
-		if (frm.doc.property) {
-			// 1) Call the server to get the source Doc and its child table
-			frappe.call({
-				method: "frappe.client.get",
-				args: {
-					doctype: "Property",
-					name: frm.doc.property,
-				},
-				callback: function (r) {
-					if (r.message) {
-						let property_doc = r.message;
+	property(frm) {
+		frappe.call({
+			method: "frappe.client.get",
+			args: {
+				doctype: "Property",
+				name: frm.doc.property,
+			},
+			callback: ({ message: property_doc }) => {
+				if (!property_doc) {
+					frappe.msgprint(__("Unable to fetch Property details."));
+					return;
+				}
 
-						// 2) Clear the existing child table rows in the target doctype
-						frm.clear_table("chargeable_services");
+				// Add rows from the fetched Property's chargeable_services
+				(property_doc.chargeable_services || []).forEach(({ item, rate }) => {
+					let new_row = frm.add_child("chargeable_services");
+					Object.assign(new_row, { item, rate });
+				});
 
-						// 3) Loop through each child row in the source doctype
-						$.each(property_doc.chargeable_services || [], function (i, row_data) {
-							// 4) Add a new child row in the target child table
-							let new_row = frm.add_child("chargeable_services");
-
-							// 5) Copy fields from the source row into the new row
-							new_row.item = row_data.item;
-							new_row.rate = row_data.rate;
-
-							// ...repeat for any other fields you need...
-						});
-
-						// 6) Refresh the field so changes appear in the UI
-						frm.refresh_field("chargeable_services");
-					}
-				},
-			});
-		} else {
-			// If `property` is blank, also clear child table
-			frm.clear_table("chargeable_services");
-			frm.refresh_field("chargeable_services");
-		}
+				// Refresh the table to display the rows
+				frm.refresh_field("chargeable_services");
+			},
+		});
 	},
 });
